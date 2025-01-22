200d137ef1102647da12ddd24ba47e35b29c8d4b
Kevin Araujo <kevin_santana.araujo@hotmail.com>
2025-01-22 09:03:01 -0300

    set log level

--- main.go
+++ main.go
@@ -1,6 +1,28 @@
 package main
 
-import "github.com/kevinsantana/gosolve-recruitment-task/cmd"
+import (
+	"fmt"
+	"os"
+
+	"github.com/kevinsantana/gosolve-recruitment-task/cmd"
+	log "github.com/sirupsen/logrus"
+)
+
+func init() {
+	log.New()
+	lvl, err := log.ParseLevel(os.Getenv("LOG_LEVEL"))
+	if err != nil {
+		fmt.Println("Invalid log level specified:", err)
+		lvl = log.DebugLevel
+	}
+	fmt.Println("log level specified:", lvl)
+	log.SetLevel(lvl)
+	log.SetReportCaller(true)
+	log.SetFormatter(&log.JSONFormatter{
+		DisableTimestamp: false,
+		TimestampFormat:  "2006-01-02 15:04:05",
+	})
+}
 
 func main() {
 	cmd.Execute()
